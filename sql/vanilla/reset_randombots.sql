f6a7ba82073c3acea4ce0494ca87f446b678d3fa
celguar <celguar@gmail.com>
2021-03-25 22:32:32 +0300

    Do not reset saved strategies when reseting random bots

--- sql/vanilla/reset_randombots.sql
+++ sql/vanilla/reset_randombots.sql
@@ -31,14 +31,3 @@ CREATE TABLE `ai_playerbot_guild_tasks` (
   KEY `guildid` (`guildid`),
   KEY `type` (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `ai_playerbot_db_store`;
-
-CREATE TABLE `ai_playerbot_db_store` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `guid` bigint(20) NOT NULL,
-  `key` varchar(32) NOT NULL,
-  `value` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `guid` (`guid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
