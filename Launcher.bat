539c5672aab1ea24b5e71b029beb1a0a2417066e
SPP User <celguar@gmail.com>
2021-09-15 21:59:57 +0300

    Update 15.09.2021
     - [Bots] Fix couple of crashes
     - [Bots] Improve bots traveling
     - [Server] Place proper mysql libs
     - Other fixes

--- Launcher.bat
+++ Launcher.bat
@@ -3,7 +3,7 @@
 SET NAME=SPP - Classics Collection
 TITLE %NAME%
 set mainfolder=%CD%
-set repack_version=2.0.9
+set repack_version=2.0.10
 
 IF NOT EXIST "%mainfolder%\music.on" (
   IF NOT EXIST "%mainfolder%\music.off" (
