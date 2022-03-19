2f17bb6c8d2200f3781aed1e405d73b18b5ec285
celguar <celguar@gmail.com>
2022-03-19 11:01:24 +0300

    Update 19.03.2022
     - [Core] Improve pathfinding (fix NormalizeZ)
     - [Core] Vanilla: Fix Reckoning talent
     - [Bots] Allow alt bots to keep leader
     - [Bots] Limit auto sell/destroy/disench if bot is in real guild
     - [Bots] Don't leave real guilds
     - [Bots] Fix bots low stats after randomize
     - Other fixes

--- Launcher.bat
+++ Launcher.bat
@@ -3,7 +3,7 @@
 SET NAME=SPP - Classics Collection
 TITLE %NAME%
 set mainfolder=%CD%
-set repack_version=2.1.8
+set repack_version=2.1.9
 set maps_date=06.06.2021
 set /a website_version=4
 
@@ -333,7 +333,7 @@ set /a realm_version=2
 set /a logs_version=1
 set /a bots_version=3
 set /a website_db_version=2
-set /a core_version=3
+set /a core_version=4
 
 goto settings
 
@@ -360,7 +360,7 @@ set /a realm_version=2
 set /a logs_version=1
 set /a bots_version=3
 set /a website_db_version=2
-set /a core_version=3
+set /a core_version=4
 
 goto settings
 
@@ -387,7 +387,7 @@ set /a realm_version=2
 set /a logs_version=1
 set /a bots_version=3
 set /a website_db_version=2
-set /a core_version=3
+set /a core_version=4
 
 goto settings
 
