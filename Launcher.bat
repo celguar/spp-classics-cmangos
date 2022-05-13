727a7c01991c57b7726f5451622875005834151d
celguar <celguar@gmail.com>
2022-05-13 18:25:16 +0300

    Update 13.05.2022
     - [Core] Vanilla core update
     - [Core] Increase visibility of objects in open world
     - [Bots] Tweak AI healers
     - [Bots] Fix paladins switching auras
     - [Bots] Fix bots running to master after each mob
     - [Bots] Fix bot stuck in looting animation
     - [Bots] Tweak healing logic, start healing earlier
     - [Bots] Tweak rogues getting behind target
     - Other fixes

--- Launcher.bat
+++ Launcher.bat
@@ -3,7 +3,7 @@
 SET NAME=SPP - Classics Collection
 TITLE %NAME%
 set mainfolder=%CD%
-set repack_version=2.2.2
+set repack_version=2.2.3
 set maps_date=06.06.2021
 set "maps_date2"="06/06/2021" 
 set /a website_version=4
@@ -328,13 +328,13 @@ set worldserver=mangosd.exe
 
 set spp_update=vanilla_base
 set /a maps_version=1
-set /a world_version=7
+set /a world_version=8
 set /a chars_version=5
 set /a realm_version=2
 set /a logs_version=1
 set /a bots_version=4
 set /a website_db_version=2
-set /a core_version=8
+set /a core_version=9
 
 goto settings
 
